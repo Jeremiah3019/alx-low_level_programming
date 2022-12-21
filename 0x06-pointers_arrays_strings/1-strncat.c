ee315114bb28e0aa52b2839d90e9d4fd381ec3c3
Jeremiah3019 <raymmanson21@gmail.com>
2022-12-21 02:29:27 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - function that concatinates two strings
+ * @dest: pointer to destination string
+ * @src:pointer to a string
+ * @n: number of bytes to be concatenated
+ * Return: pointer to destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *cat = dest + _strlen(dest);
+	int length;
+
+	if (n > _strlen(src) + _strlen(dest))
+		length =  _strlen(dest) + _strlen(src);
+	else
+		length = _strlen(dest) + n;
+	while (*src && n > 0)
+	{
+		*cat += *src;
+		src++;
+		cat++;
+		n--;
+	}
+	if (n > 0)
+		*cat += '\0';
+	cat -= (length);
+	*dest = *cat;
+	return (cat);
+}
