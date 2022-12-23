27fa7b56e4939501990f622e660a0c04336d22ab
Jeremiah3019 <raymmanson21@gmail.com>
2022-12-23 06:37:46 -0800

    Magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -15,5 +23,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _strlen(char *);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif
+#endif /*MAIN_H*/
