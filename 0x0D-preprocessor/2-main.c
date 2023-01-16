a1f9af28284ddc26d9c02d26fc33bd4067613923
Jeremiah3019 <raymmanson21@gmail.com>
2023-01-16 02:10:29 -0800

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,17 +2,11 @@
 
 /**
  *
- *  * main - prints the name of the file it was compiled from
- *
- *   * Return: 0;
- *
- *    */
-
+ * main - prints the name of the file it was compiled from
+ * Return: 0;
+ */
 int main(void)
 {
-
-		printf("%s\n", __FILE__);
-
-			return (0);
-
+	printf("%s\n", __FILE__);
+	return (0);
 }
